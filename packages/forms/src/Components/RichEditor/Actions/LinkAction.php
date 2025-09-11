38ba8bc23b260be7a37ec501f51de0c79ceb3611
alwayshopeless <38844539+alwayshopeless@users.noreply.github.com>
2025-09-11 04:34:22 +0300

    Update LinkAction.php.  Allow relative links
    Allow relative links in URL

--- packages/forms/src/Components/RichEditor/Actions/LinkAction.php
+++ packages/forms/src/Components/RichEditor/Actions/LinkAction.php
@@ -23,8 +23,7 @@ class LinkAction
             ])
             ->schema([
                 TextInput::make('url')
-                    ->label(__('filament-forms::components.rich_editor.actions.link.modal.form.url.label'))
-                    ->url(),
+                    ->label(__('filament-forms::components.rich_editor.actions.link.modal.form.url.label')),
                 Checkbox::make('shouldOpenInNewTab')
                     ->label(__('filament-forms::components.rich_editor.actions.link.modal.form.should_open_in_new_tab.label')),
             ])
